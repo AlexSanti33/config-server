de435854185ee37bded4611f412a3c9b8ec9b5bc
AlexSanti33 <santiago.jav27@gmail.com>
2024-06-11 21:27:38 -0600

    Se dockeriza microservicio

--- src/test/java/com/portafolio/micros/config_server/ConfigServerApplicationTests.java
+++ src/test/java/com/portafolio/micros/config_server/ConfigServerApplicationTests.java
@@ -3,10 +3,8 @@ package com.portafolio.micros.config_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ConfigServerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
